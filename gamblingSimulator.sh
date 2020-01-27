dfba98197575a860e2fb8d30d7b5100088caccfe
gajanan15 <gajanandesai97@gmail.com>
2020-01-27 12:12:25 +0530

    ADDED: checked resigning condition using while loop

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,12 +2,25 @@
 
 echo "Welcome To Gambling Simulator"
 
+WINNING_LIMIT=150
+LOOSING_LIMIT=50
 BET=1
 stake=100
 
-if [ $((RANDOM%2)) -eq 1 ]
+cashInHand=$stake
+while [[ $cashInHand -lt $WINNING_LIMIT && $cashInHand -gt $LOOSING_LIMIT ]]
+do
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		cashInHand=$(($cashInHand + $BET))
+	else
+		cashInHand=$(($cashInHand - $BET))
+	fi
+done
+
+if [ $cashInHand -eq $WINNING_LIMIT ]
 then
-	stake=$(($stake + $BET))
+	echo "Gambler Won And Resign for the day"
 else
-	stake=$(($stake - $BET))
+	echo "Gambler Loose And Resign for the day"
 fi
